f41e0ff8c7a45e08a25d973900b376fa27581b64
r.witczyk <r.witczyk@wp.pl>
2018-12-11 21:07:33 +0100

    [Added connection between 2 nodes]

---
+++ Traffic simulator/Assets/Scripts/Collision_Node_setEdit.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Collision_Node_setEdit : MonoBehaviour
+{
+    string nazwa_obiektu = "";
+
+    //IEnumerator
+    void OnTriggerEnter(Collider collison)
+    {
+
+        nazwa_obiektu = collison.gameObject.name;
+       // Debug.Log(nazwa_obiektu);
+
+        if (collison.gameObject.name.Substring(0, 14) == "PedestrianNode") //dziala
+        {
+            Debug.Log(collison.gameObject.name);
+            Debug.Log("Jestem w setEdit");
+
+            //jezeli ma byc kotwica
+            //PedestrianSystem.Instance.SetPedestrianNode(PedestrianSystem.Tooltip.ANCHOR, PedestrianNode);
+
+            //jezeli ma byc klucz
+            PedestrianSystem.Instance.SetPedestrianNode(PedestrianSystem.Tooltip.EDIT, gameObject.GetComponent(typeof(PedestrianNode)) as PedestrianNode);
+
+            //yield return new WaitForSeconds(0.1f);
+
+            PedestrianSystem.Instance.LinkNode();
+        }
+
+     
+    }
+
+}
